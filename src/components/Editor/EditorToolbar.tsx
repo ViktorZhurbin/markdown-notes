df3c4944c1bb8465781bf4445ba6b593f597fab6
Viktor Zhurbin <viktor.zhurbin@pumble.com>
2026-02-19 11:45:45 +0100

    fix sticky toolbar on mobile

--- src/components/Editor/EditorToolbar.tsx
+++ src/components/Editor/EditorToolbar.tsx
@@ -1,45 +1,77 @@
 import { RichTextEditor } from "@mantine/tiptap";
+import { useEffect, useRef } from "react";
 import { CopyMdButton } from "../CopyMdButton";
 import { MoreOptionsMenu } from "../MoreOptionsMenu";
 import styles from "./EditorToolbar.module.css";
 
-export const EditorToolbar = ({ entryId }: { entryId: string }) => (
-  <RichTextEditor.Toolbar sticky className={styles.toolbar}>
-    <RichTextEditor.ControlsGroup>
-      <CopyMdButton />
-      <MoreOptionsMenu entryId={entryId} />
-    </RichTextEditor.ControlsGroup>
+export const EditorToolbar = ({ entryId }: { entryId: string }) => {
+  const toolbarRef = useRef<HTMLDivElement>(null);
 
-    <RichTextEditor.ControlsGroup>
-      <RichTextEditor.Undo />
-      <RichTextEditor.Redo />
-    </RichTextEditor.ControlsGroup>
+  /* for sticky toolbar to work on mobile with virtual keyboard on */
 
-    <RichTextEditor.ControlsGroup>
-      <RichTextEditor.H1 />
-      <RichTextEditor.H2 />
-      <RichTextEditor.H3 />
-      <RichTextEditor.H4 />
-    </RichTextEditor.ControlsGroup>
+  useEffect(() => {
+    if (!window.visualViewport) return;
 
-    <RichTextEditor.ControlsGroup>
-      <RichTextEditor.Bold />
-      <RichTextEditor.Code />
-      <RichTextEditor.Italic />
-      <RichTextEditor.Underline />
-      <RichTextEditor.Strikethrough />
-    </RichTextEditor.ControlsGroup>
+    const handleViewportChange = () => {
+      if (toolbarRef.current && window.visualViewport !== null) {
+        const offset = window.visualViewport.offsetTop;
+        toolbarRef.current.style.top = `${offset}px`;
+      }
+    };
 
-    <RichTextEditor.ControlsGroup>
-      <RichTextEditor.Blockquote />
-      <RichTextEditor.Hr />
-      <RichTextEditor.BulletList />
-      <RichTextEditor.OrderedList />
-    </RichTextEditor.ControlsGroup>
+    window.visualViewport.addEventListener("resize", handleViewportChange);
+    window.visualViewport.addEventListener("scroll", handleViewportChange);
 
-    <RichTextEditor.ControlsGroup>
-      <RichTextEditor.Link />
-      <RichTextEditor.Unlink />
-    </RichTextEditor.ControlsGroup>
-  </RichTextEditor.Toolbar>
-);
+    return () => {
+      window.visualViewport?.removeEventListener(
+        "resize",
+        handleViewportChange,
+      );
+      window.visualViewport?.removeEventListener(
+        "scroll",
+        handleViewportChange,
+      );
+    };
+  }, []);
+
+  return (
+    <RichTextEditor.Toolbar sticky ref={toolbarRef} className={styles.toolbar}>
+      <RichTextEditor.ControlsGroup>
+        <CopyMdButton />
+        <MoreOptionsMenu entryId={entryId} />
+      </RichTextEditor.ControlsGroup>
+
+      <RichTextEditor.ControlsGroup>
+        <RichTextEditor.Undo />
+        <RichTextEditor.Redo />
+      </RichTextEditor.ControlsGroup>
+
+      <RichTextEditor.ControlsGroup>
+        <RichTextEditor.H1 />
+        <RichTextEditor.H2 />
+        <RichTextEditor.H3 />
+        <RichTextEditor.H4 />
+      </RichTextEditor.ControlsGroup>
+
+      <RichTextEditor.ControlsGroup>
+        <RichTextEditor.Bold />
+        <RichTextEditor.Code />
+        <RichTextEditor.Italic />
+        <RichTextEditor.Underline />
+        <RichTextEditor.Strikethrough />
+      </RichTextEditor.ControlsGroup>
+
+      <RichTextEditor.ControlsGroup>
+        <RichTextEditor.Blockquote />
+        <RichTextEditor.Hr />
+        <RichTextEditor.BulletList />
+        <RichTextEditor.OrderedList />
+      </RichTextEditor.ControlsGroup>
+
+      <RichTextEditor.ControlsGroup>
+        <RichTextEditor.Link />
+        <RichTextEditor.Unlink />
+      </RichTextEditor.ControlsGroup>
+    </RichTextEditor.Toolbar>
+  );
+};
